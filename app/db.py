1b83be0f427e4a563b81f83bd127a8bf542d1e5e
alswooy <108874185+alswooy@users.noreply.github.com>
2024-09-03 15:54:07 +0900

    db, utils

--- app/db.py
+++ app/db.py
@@ -1,11 +1,11 @@
+# DB 관련 파일
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
 import json
 load_dotenv()
 
-user = "admin@"
-
+# DB 연결
 def get_db_connections():
     envhost = os.getenv('DB_HOST')
     envuser = os.getenv('DB_USER')
@@ -20,6 +20,7 @@ def get_db_connections():
         print(f"Failed to connect to the database: {e}")
         return None
 
+# JSON형식으로 변경하는 함수
 def execute_query(query, engine, params=None):
     """주어진 쿼리를 실행하고 결과를 JSON 형식으로 반환하는 함수"""
     try:
@@ -30,16 +31,25 @@ def execute_query(query, engine, params=None):
     except Exception as e:
         print(f"Failed to execute query: {e}")
         return json.dumps([])
-    
- #주문 정보 Json형태로 출력
+
+# 쿼리문 작성
 def get_order_db(email):
     engine = get_db_connections()
-    
     if engine is None:
         return json.dumps([])
-
     # 쿼리 작성
     query = text("SELECT or_no, or_prices, or_delvs, or_date FROM orders WHERE c_email= :email")
     
-    # 쿼리 실행
+    # 쿼리 실행 email이 맞는 정보만 출력
     return execute_query(query, engine, {'email': email})
+
+def get_cust_db(email, password):
+    engine = get_db_connections()
+    if engine is None:
+        return json.dumps([])
+    # 쿼리 작성
+    query = text("select ")
+    
+    # 쿼리 실행 email이 맞는 정보만 출력
+    return execute_query(query, engine, {'email': email, 'password':password})
+    
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,32 @@
-from flask import Blueprint, render_template, jsonify
-# from app.db import get_cust_db, get_notice_db, get_faq_db, get_order_db, get_product_db, get_cart_db
+# API 파일
+from flask import Blueprint, request, render_template, jsonify
 from flask_cors import CORS
 from app.db import get_order_db
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+from utils import (
+    make_prompt, 
+    extract_purchase_id,
+    extract_customer_name_email
+)
+load_dotenv()
 
 main_bp = Blueprint('main', __name__)
 CORS(main_bp)
+main_bp.secret_key = os.getenv('SECRET_KEY')
 
-@main_bp.route('/')
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=api_key)
+
+messages = []
+
+
+@main_bp.route('/', methods=["GET", "POST"])
 def index():
-    email = "admin@"
-    data = get_order_db(email)
-    print(data)
-    return data
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        if '구매내역' in user_input:
+            email = "admin@"
+            user = get_order_db(email) # DB 쿼리문 json으로 호출
+        
